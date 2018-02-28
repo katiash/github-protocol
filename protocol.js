1cc3b0dff680b31436de442327e70ed44c174340
Omar Ihmoda <omarihmoda@Omars-MacBook-Pro.local>
2018-02-28 08:54:17 -0800

    initial commit

---
+++ protocol.js
@@ -0,0 +1,20 @@
+//This document outlines the protocol for making a git repository.
+//We will also be using this document to test our ability to successfully merge files in Git!
+//Once we have all added our names to this file using version control, we can feel confident that our protocol makes sense!
+
+
+/*
+
+Step 1: Fork this repository. Click the "fork button" to create a remote copy of this branch in your Github account.
+Step 2: Clone your remote fork of this branch to your local folder.
+
+
+
+
+
+
+
+
+*/
+
+var contributors = [];
